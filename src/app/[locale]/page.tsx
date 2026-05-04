449a45676a579b4c361ee5d98c51a95ec3bc6778
Sefo Nodzimo <Sefo@Nodzimo.com>
2026-05-04 13:04:39 +0300

    feat: Тестовая страница

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,18 @@
 import { useLocale, useTranslations } from 'next-intl'
+import { Link } from '@/i18n/navigation'
 
 export default function IndexPage() {
-  const t = useTranslations('IndexPage')
+  const t = useTranslations()
   const locale = useLocale()
   const { region } = new Intl.Locale(locale)
   console.debug(`[SN] Locale: ${locale}, Region: ${region}`)
 
   return (
-    <div>
-      <h1 className={'mb-2 font-bold text-2xl'}>{t('title')}</h1>
+    <div className={'flex flex-col gap-2'}>
+      <Link href={'/test'}>{t('Navigation.testPage')}</Link>
+      <h1 className={'font-bold text-2xl'}>{t('IndexPage.title')}</h1>
       <blockquote className={'whitespace-pre-line'}>
-        {t('description')}
+        {t('IndexPage.description')}
       </blockquote>
     </div>
   )
---
+++ src/app/[locale]/test/page.tsx
@@ -0,0 +1,31 @@
+import { useTranslations } from 'next-intl'
+import { Link } from '@/i18n/navigation'
+
+export default function TestPage() {
+  const t = useTranslations()
+
+  return (
+    <div className={'flex flex-col gap-10'}>
+      <Link href={'/'}>{t('Navigation.indexPage')}</Link>
+      <p>{t('TestPage.pangram')}</p>
+      <p>{t('TestPage.alphabetUppercase')}</p>
+      <p>{t('TestPage.alphabetLowercase')}</p>
+      <p>0 1 2 3 4 5 6 7 8 9</p>
+      <p>ilI1L g oO0Q СФ</p>
+      <p>
+        :D :-) ^^ : ) TT :) :o :O :0 :P xD :| :/ :\ :3 {'>'}:E o_O 0_0 ))) {'<'}
+        3 .!. :'(
+      </p>
+      <p>₿ ₽ ₹ $ ¢ € £ ¥ ¤</p>
+      <p>☭ © ® ™ ´ · ˆ ˚ ˜</p>
+      <p>⌀ ∞ √ ° % ¹ ² ³ ¼ ½ ¾ + - = ≠ × ÷ ±</p>
+      <p>{'< > { }'}</p>
+      <p>~ / @ [ \ ] | ^ ¦ _ `</p>
+      <p>§ • … ? ... ! " # № & ' ( ) * , . : ;</p>
+      <p>‐ ‑ ‒ – —</p>
+      <p>‹ › « » ‘ ’ ‚ “ ” „</p>
+      <p>← → ↑ ↓ ▲ ▼ ↻ ↺</p>
+      <p>✓ ✔ 🗸 ✖ ✗ ✘ ☒</p>
+    </div>
+  )
+}
